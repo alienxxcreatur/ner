8fee8f7c3a11786046d9809a72b2cbba961031ee
alienxxcreatur <alienxxcreatur@gmail.com>
2025-12-12 01:10:13 +0330

    bilstm fix2

--- train_ncbi.sh
+++ train_ncbi.sh
@@ -1,17 +1,17 @@
-export MODEL=bert-base-cased    # برای شروع، همین مدل عمومی را استفاده کن
-epoch=5                         # برای تست اولیه 5 اپوک کافی است
+export MODEL=bert-base-cased  # برای شروع، همین مدل عمومی را استفاده کن
+epoch=5  # برای تست اولیه 5 اپوک کافی است
 lr=5e-5
 wis=1qq3qq5qq7
 data_type=ncbi
 connect_type=dot-att
 
-CUDA_VISIBLE_DEVICES=0 python run_hgn.py \
-  --train_data_dir=data/$data_type/train_merge.txt \
-  --dev_data_dir=data/$data_type/dev.txt \
-  --test_data_dir=data/$data_type/test.txt \
+CUDA_VISIBLE_DEVICES=0 python /content/hgn_project/run_hgn.py \
+  --train_data_dir=/content/hgn_project/data/$data_type/train_merge.txt \
+  --dev_data_dir=/content/hgn_project/data/$data_type/dev.txt \
+  --test_data_dir=/content/hgn_project/data/$data_type/test.txt \
   --bert_model=${MODEL} \
   --task_name=ner \
-  --output_dir=./output/ncbi_hgner \
+  --output_dir=/content/hgn_project/output/ncbi_hgner \
   --max_seq_length=128 \
   --num_train_epochs ${epoch} \
   --do_train \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 python run_hgn.py \
   --learning_rate ${lr} \
   --warmup_proportion=0.1 \
   --train_batch_size=16 \
-  --use_bilstm \
+  --use_bilstm True \  # اضافه کردن مقدار True به پارامتر use_bilstm
   --use_multiple_window \
   --windows_list=${wis} \
   --connect_type=${connect_type} \
